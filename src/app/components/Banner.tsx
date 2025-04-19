37c15b51177c8fc23c9bc8d9a48466082c1e0a46
Jorge Luis Sanchez Mendoza <57418730+jorgeula300@users.noreply.github.com>
2025-04-19 14:07:56 -0500

    error corregido

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,10 +1,9 @@
 
-
 const Banner = () => {
     return (
         <section className="w-full max-w-3xl mx-auto space-y-3 px-2">
             <div className="w-full  max-w-3xl h-52 relative mx-auto overflow-hidden rounded-2xl shadow">
-                <img className="w-full h-full object-cover" src="/bank.png" alt="bank" />
+                <img className="w-full h-full object-cover" src="/Bank.png" alt="bank" />
                 <div className=" absolute top-0 left-0 w-full h-full bg-[#0003] backdrop-blur-[0.8px] px-2 flex flex-col justify-center text-white ">
                     <div className="w-full flex flex-col justify-center items-center gap-2 ">
                         <h2 className="text-2xl font-bold  text-principal ">Bienvenido a FinCo</h2>
